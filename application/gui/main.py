e5a5baded02ee4c9105845f4e11f71288b497d63
Josh Dunkley <jdunkley@vols.utk.edu>
2020-01-14 11:58:39 -0500

    Main directory for application contents

---
+++ application/gui/main.py
@@ -0,0 +1,11 @@
+from PyQt5.QtWidgets import*
+from PyQt5.QtCore import*
+from PyQt5.QtGui import*
+
+class SNR(MainWindow):
+    def __init__():
+        pass
+
+
+if __name__=="__main__":
+    print("Main application file.")
